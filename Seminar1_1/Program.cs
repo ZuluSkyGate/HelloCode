acb6a948bdc364dd529f976cb5f113f2f7b1144e
ZuluSkyGate <124462623+ZuluSkyGate@users.noreply.github.com>
2023-12-03 21:14:52 +0300

    add Seminar1_1

---
+++ Seminar1_1/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Enter number_1: ");
+string str_1 = Console.ReadLine();
+int num_1 = Convert.ToInt32(str_1);
+System.Console.WriteLine("Enter number_2: ");
+string str_2 = Console.ReadLine();
+int num_2 = Convert.ToInt32(str_2);
+if(num_2 * num_2 == num_1)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
