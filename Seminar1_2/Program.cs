b50936d8bf2e77e51a47c01ad33715048a94e023
ZuluSkyGate <124462623+ZuluSkyGate@users.noreply.github.com>
2023-12-03 21:55:43 +0300

    add Seminar1_2

---
+++ Seminar1_2/Program.cs
@@ -0,0 +1,35 @@
+﻿System.Console.WriteLine("Enter week day: ");
+string week_day = Console.ReadLine();
+int num_week_day = Convert.ToInt32(week_day);
+if(num_week_day == 1)
+{
+    Console.WriteLine("Monday");
+}
+else if (num_week_day == 2)
+{
+        Console.WriteLine("Tuesday");
+}
+else if (num_week_day == 3)
+{
+        Console.WriteLine("Wednesday");
+}
+else if (num_week_day == 4)
+{
+        Console.WriteLine("Thursday");
+}
+else if (num_week_day == 5)
+{
+        Console.WriteLine("Friday");
+}
+else if (num_week_day == 6)
+{
+        Console.WriteLine("Saturday");
+}
+else if (num_week_day == 7)
+{
+        Console.WriteLine("Sunday");
+}
+else
+{
+    Console.WriteLine("Uncorrect number");
+}
