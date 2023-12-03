1385bcd7ab392c9f4c4e51f93f2400918194f64a
ZuluSkyGate <124462623+ZuluSkyGate@users.noreply.github.com>
2023-12-03 20:57:33 +0300

    add Seminar1

---
+++ Seminar1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат(число умноженное на само себя).
+// Например: 4 -> 16, -3 -> 9, -7 -> 49
+
+System.Console.WriteLine("Enter number: ");
+string str = Console.ReadLine();
+int num = Convert.ToInt32(str);
+
+int square = num * num;
+System.Console.WriteLine($" square = {square}");
