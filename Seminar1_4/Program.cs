c391d5592b54544f2fbd70c50b02b1330e3e6405
ZuluSkyGate <124462623+ZuluSkyGate@users.noreply.github.com>
2023-12-03 22:49:41 +0300

    add Seminar1_4

---
+++ Seminar1_4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter number: ");
+string str = Console.ReadLine();
+int num = Convert.ToInt32(str);
+if(num > 99 && num < 1000)
+{
+    int num2 = num % 10;
+    Console.WriteLine(num2);
+}
+else
+{
+    Console.WriteLine("Uncorrect number");
+}
