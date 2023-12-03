b86d849c6215c8f1de272e5b8a57cd97a3d85d70
ZuluSkyGate <124462623+ZuluSkyGate@users.noreply.github.com>
2023-12-03 14:39:41 +0300

    add Seminar2_1

---
+++ Seminar2_1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+int number = new Random().Next(100, 1000);
+int num1 = number / 100;
+int num2 = number % 100;
+if result = num1 * 10 + num2;
+
+Console.WriteLine(number);
+Console.WriteLine(result);
