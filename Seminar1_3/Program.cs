6a25d53c09d64c09a8e9511c929e5d213e1227c0
ZuluSkyGate <124462623+ZuluSkyGate@users.noreply.github.com>
2023-12-03 22:16:01 +0300

    add Seminar1_3

---
+++ Seminar1_3/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.WriteLine("Enter number: ");
+string number = Console.ReadLine();
+int num = Convert.ToInt32(number);
+int numStart = num * -1;
+while (numStart <= num)
+    {
+        Console.WriteLine(numStart);
+        ++numStart;
+    }    
