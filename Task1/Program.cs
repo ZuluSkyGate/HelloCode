6682f744f690762fc544800990016171d0903871
ZuluSkyGate <124462623+ZuluSkyGate@users.noreply.github.com>
2023-12-06 20:40:49 +0300

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Answer
+{
+    static void CompareNumbers(int firstNumber, int secondNumber)
+    {
+        if (firstNumber > secondNumber)
+        {
+            Console.WriteLine($"первое число '{firstNumber}' больше чем второе число '{secondNumber}'");
+        }
+        else if (firstNumber < secondNumber)
+        {
+            Console.WriteLine($"первое число '{firstNumber}' меньше чем второе число '{secondNumber}'");
+        }
+        else
+        {
+            Console.WriteLine($"первое число '{firstNumber}' равно второму числу '{secondNumber}'");
+        }
+    }
+
+    public static void Main()
+    {
+        Console.Write("Введите первое число: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите второе число: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        CompareNumbers(num1, num2);
+    }
+}
