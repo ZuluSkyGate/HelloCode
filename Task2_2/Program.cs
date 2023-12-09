e801698dcac4b6bb817f40c106882683791a630e
ZuluSkyGate <124462623+ZuluSkyGate@users.noreply.github.com>
2023-12-09 21:25:16 +0300

    add Task2_2

---
+++ Task2_2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if ((x > 0) && (y > 0))
+{
+    Console.Write("1");
+}
+if ((x < 0) && (y > 0))
+{
+    Console.Write("2");
+}
+if ((x < 0) && (y < 0))
+{
+    Console.Write("3");
+}
+if ((x > 0) && (y < 0))
+{
+    Console.Write("4");
+}
