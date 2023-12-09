f7b056acf003c15b591cad726bd24f374e66ad3e
ZuluSkyGate <124462623+ZuluSkyGate@users.noreply.github.com>
2023-12-09 21:44:07 +0300

    add Task2_3

---
+++ Task2_3/Program.cs
@@ -0,0 +1,15 @@
+﻿int number = new Random().Next(10, 100);
+
+Console.WriteLine($"Случайное число из отрезка [10, 99] = {number}");
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine($"Наибольшая цифра этого числа: {firstDigit}");
+}
+else
+{
+    Console.WriteLine($"Наибольшая цифра этого числа: {secondDigit}");
+}
