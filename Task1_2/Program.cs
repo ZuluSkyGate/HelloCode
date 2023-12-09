cf8802c25ae7b71aea104f4632657e4a863315bd
ZuluSkyGate <124462623+ZuluSkyGate@users.noreply.github.com>
2023-12-09 12:28:26 +0300

    add code Task1_2

---
+++ Task1_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Answer {
+    
+  static int FindMax(int a, int b, int c)
+    {
+      // Введите свое решение ниже
+        
+        int max = a;
+
+        if (b > max)
+        {
+            max = b;
+        }
+
+        if (c > max)
+        {
+            max = c;
+        }
+
+        return max;
+    }
+
+  
+        System.Console.WriteLine($"{result}");
+    }
+}
