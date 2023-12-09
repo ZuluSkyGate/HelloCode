d812c5c983546c6d45eaae25d9efdf0c91041e05
ZuluSkyGate <124462623+ZuluSkyGate@users.noreply.github.com>
2023-12-09 12:31:14 +0300

    add code Task1_4

---
+++ Task1_4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Answer {
+    static void PrintEvenNumbers(int number)
+    {
+      // Введите свое решение ниже
+            int i = 2;
+            while (i <= number)
+        {
+                  Console.Write(i);
+                  i += 2;
+
+                  if (i <= number)
+            {
+                  Console.Write('\t');
+            }
+   
+        }
+        Console.WriteLine();
+
+    }
+
+
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 6;
+        }
+
+        // Не удаляйте строки ниже
+        PrintEvenNumbers(number);
+    }
+}
