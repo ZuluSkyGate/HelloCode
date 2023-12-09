2b804674d9bb712cf8f2ff42dd458be434e13f87
ZuluSkyGate <124462623+ZuluSkyGate@users.noreply.github.com>
2023-12-09 12:30:53 +0300

    add code Task1_3

---
+++ Task1_3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Answer {
+    static void CheckIfEven(int number) {
+        if (number % 2 == 0) {
+            Console.WriteLine($"Число `{number}` чётное");
+        } else {
+            Console.WriteLine($"Число `{number}` нечётное");
+        }
+    }
+static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 7;
+        }
+
+        // Не удаляйте строки ниже
+        CheckIfEven(number);
+    }
+}
