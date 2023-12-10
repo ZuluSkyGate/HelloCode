56cd2e86bb897c8bda6d6457733125a7b78cc48d
ZuluSkyGate <124462623+ZuluSkyGate@users.noreply.github.com>
2023-12-10 23:16:23 +0300

    add commit Seminar3_1

---
+++ Seminar3_1/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задайте массив. Напишите программу, которая определяет,
+// присутствует ли заданное число в массиве.
+// Программа должна выдать ответ: Да/Нет.
+
+int[] array = new int[10];
+
+Console.WriteLine("Введите число: ");
+
+FillArray(array);
+PrintArray(array);
+
+int number = Convert.ToInt32(Console.Read);
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+
+bool FindNumber(int number, int[] list)
+{
+    for (int i = 0; list.length; i++)
+    {
+        if (number == list[i])
+        {
+            return true;
+        }
+        else
+    }
+
+    {
+        return false;
+    }
+
+    if (FindNumber{ number, array});
+
+    {
+        Console.WriteLine("Да");
+    }
+
+}
+
+void PrintArray(int[] col)
+int count = col.Length;
+int position = 0;
+while (position < count)
+{
+    Consol.WriteLine(col[position] + "");
+    position++;
+}
+
