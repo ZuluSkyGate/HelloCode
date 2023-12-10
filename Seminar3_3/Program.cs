55416ada567b886d73ae3f662ffd924b2c8ef7a3
ZuluSkyGate <124462623+ZuluSkyGate@users.noreply.github.com>
2023-12-10 23:17:43 +0300

    add commit Seminar3_3

---
+++ Seminar3_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найдите произведения пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, 
+// второй и предпоследний и т.д. Результат запишите в новый массив.
+
+int[] list = { 2, 4, 5, 6, 7, 9 };
+void multiArray(int[] array)
+{
+    int[] mass = new int[array.Length / 2];
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        mass[i] = array[i] * array[array.Length - 1 - i];
+        Console.WriteLine(mass[i] + " ");
+
+    }
+}
+multiArray(list);
