8c2a5d5d4a3a1ea9077c4d89691adf0b4fdf35a1
ZuluSkyGate <124462623+ZuluSkyGate@users.noreply.github.com>
2023-12-10 23:17:09 +0300

    add commit Seminar3_2_1

---
+++ Seminar3_2_1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Определите количество простых чисел в этом массиве.
+// Примеры [1 3 4 19 3] => 2 [4 3 4 1 9 5 21 13]  => 3
+
+int [] list = new int [10];
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+FillArray(list);
+
+bool IsPrime(int num)
+{
+    for (int i = 2; i < num; i++)
+    {
+        if (num % i == 0)
+        return false;
+    }
+    return true;
+}
+
+
+int GetCountPrimeNumber(int [] array )
+{
+    int count = 0;
+    for (int i = 0; i < array.length; i++)
+    {
+        if (IsPrime(array[i]))
+        count++;
+    }
+}   return count;
+
+
+
+Console.WriteLine(GetCountPrimeNumber(list));
