d8698033f93e810cabaeee3f8170f6b51e105cc0
ZuluSkyGate <124462623+ZuluSkyGate@users.noreply.github.com>
2023-12-10 23:16:49 +0300

    add commit Seminar3_2

---
+++ Seminar3_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте массив из 10 элементов, заполненный числами из промежутка [-10, 10].
+// Замените отрицательные элементы на положительные, а положительные на отрицательные.
+
+void FillArray(int[] collection)
+void PrintArray(int[] col)
+void PositiveArray(int[] pos)
+{
+    for (int i = 0; i < pos.Length; i++)
+    {
+        pos[i] *= -1;
+    }
+}
+
+
+
+int list = new int[10];
+FillArray(list);
+PrintArray(list);
+PositiveArray(list);
+
