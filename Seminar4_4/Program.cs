380bffb1f3658d39b0a6a1701af6ce9755f0f611
ZuluSkyGate <124462623+ZuluSkyGate@users.noreply.github.com>
2023-12-19 21:02:11 +0300

    add Seminar4_4

---
+++ Seminar4_4/Program.cs
@@ -0,0 +1,26 @@
+﻿// Обратная строка: Создайте программу, которая принимает строку
+// и выводит её в обратном порядке. Например, "hello" станет "olleh".
+
+string GetString()
+{
+    Console.Write("Введите строку: ");
+    string str = Console.ReadLine();
+    return str;
+}
+string ReversString(string str)
+{
+    string result = String.Empty;
+    for (int i = str.Length - 1; i >= 0; i--)
+    {
+        result += str[i];
+    }
+    return result;
+}
+string str = GetString();
+string rev = ReversString(str);
+
+Console.WriteLine(rev);
+
+
+
+
