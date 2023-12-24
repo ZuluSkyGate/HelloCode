f0822ded389dff1d6e66f550f2e51c5ee4a994b5
ZuluSkyGate <124462623+ZuluSkyGate@users.noreply.github.com>
2023-12-24 18:20:26 +0300

    add Seminar5_3

---
+++ Seminar5_3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+// в промежутке от 1 до N.
+
+int N = GetNumber();
+listNumber(N);
+
+int GetNumber()
+{
+    Console.WriteLine("Введите число N: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void listNumber(int num, int start = 1)
+{
+    if (start <= N)
+    {
+        Console.WriteLine(start);
+        listNumber(num, start + 1);
+    }
+}
