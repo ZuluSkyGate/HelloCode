0dff8304aa3b80115ab167c86f95eeda742d1987
ZuluSkyGate <124462623+ZuluSkyGate@users.noreply.github.com>
2023-12-24 18:12:53 +0300

    add Seminar5_1

---
+++ Seminar5_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+int number = GetNumber();
+
+static int GetNumber()
+{
+    Console.WriteLine("Введите чисо: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int sum = FindSum(number);
+
+int FindSum(int number)
+{
+    if(number == 0)
+    {
+        return 0;
+    }
+    int sum = number%10 + FindSum(number/10);
+    return sum;
+}
+Console.WriteLine(sum);
