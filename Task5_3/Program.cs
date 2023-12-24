72df108d3620415cd5d4551f46192eb30641eda5
ZuluSkyGate <124462623+ZuluSkyGate@users.noreply.github.com>
2023-12-24 20:19:47 +0300

    add Task5_3

---
+++ Task5_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца.
+// Использовать рекурсию, не использовать циклы.
+
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+PrintArrayReverse(array, array.Length - 1);
+
+void PrintArrayReverse(int[] arr, int index)
+{
+    if (index < 0)
+        return;
+
+    Console.WriteLine(arr[index]);
+    PrintArrayReverse(arr, index - 1);
+}
