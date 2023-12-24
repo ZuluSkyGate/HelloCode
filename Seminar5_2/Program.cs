924831bcb564b8d0e1ee27ac3cf273ce29a9915a
ZuluSkyGate <124462623+ZuluSkyGate@users.noreply.github.com>
2023-12-24 18:30:43 +0300

    add Seminar5_2

---
+++ Seminar5_2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Считать строку с консоли, содержащую латинские буквы. 
+// Вывести на экран согласные буквы этой строки.
+
+string str = GetString();
+PrintString(str);
+
+string GetString()
+{
+    Console.WriteLine("Введите строку: ");
+    string list = Console.ReadLine();
+    return list;
+}
+
+bool IsVowel(char c)
+{
+    return c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' ||
+           c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+}
+
+void PrintString(string str, int index = 0)
+{
+    if (index < str.Length)
+    {
+        char c = Char.ToUpper(str[index]);
+        if ((c >= 'A' && c <= 'Z') && !IsVowel(c)) // ! - превращает истину в лож, а лож - в истину
+        {
+            Console.Write(c + " ");
+        }
+        PrintString(str, index + 1);
+    }
+}
