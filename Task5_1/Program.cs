f83693cf06172aa0f102174e8c485c746e1f3ab5
ZuluSkyGate <124462623+ZuluSkyGate@users.noreply.github.com>
2023-12-24 20:19:04 +0300

    add Task5_1

---
+++ Task5_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа
+// в промежутке от M до N.
+// Использовать рекурсию, не использовать циклы.
+
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+void num(int m, int n)
+{
+    if (m > n)
+        return;
+    if (m % 2 == 0)
+    {
+        Console.Write($"{ m}, ");
+    }
+    num(m + 1, n);
+
+}
+num(m, n);
